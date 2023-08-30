db0cbf66ff91e13af72dfed7a83f694371c154b7
mataras666 <106777275+mataras666@users.noreply.github.com>
2023-08-30 14:44:16 +0300

    updated js

--- js/index.esm.js
+++ js/index.esm.js
@@ -5,6 +5,8 @@
  * --------------------------------------------------------------------------
  */
 
+console.log("my contrubition");
+
 export { default as Alert } from './src/alert.js'
 export { default as Button } from './src/button.js'
 export { default as Carousel } from './src/carousel.js'
